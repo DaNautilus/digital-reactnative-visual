cffbb9b5e18b7a101935f6f01546ec9f6fcfa74f
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-09-21 11:09:37 +0200

    swipeable with pure text instead of icon

--- js/atoms/Swipeable.js
+++ js/atoms/Swipeable.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import Swipeable from 'react-native-swipeable';
 
 import Icon from './Icon';
+import { Text } from './Typography';
 
 import { getColor } from '../utils/colors';
 
@@ -23,6 +24,28 @@ return (
   )
 }
 
+export function SwipeableText({ children, onPress, left, ...props }) {
+const backgroundColor = getColor(props);
+
+return (
+    <TouchableOpacity onPress={onPress} style={{
+      flex: 1,
+      justifyContent: 'center',
+      paddingLeft: left ? 0 : 12,
+      paddingRight: left ? 12 : 0,
+      alignItems: left ? 'flex-end' : 'flex-start',
+      backgroundColor
+    }}>
+      <View style={{ width: 75, justifyContent: 'center', flexDirection: 'row' }}>
+        <Text ellipsizeMode='tail' numberOfLines={2} white style={{ fontSize: 12 }}>
+          {children}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
 Swipeable.Icon = SwipeableIcon;
+Swipeable.Text = SwipeableText;
 
 export default Swipeable;
--- js/atoms/Swipeable.stories.js
+++ js/atoms/Swipeable.stories.js
@@ -12,6 +12,11 @@ import Swipeable from './Swipeable';
 
 import * as colors from '../colors';
 
+function Sep() {
+  return (
+    <View style={{ backgroundColor: colors.borderGray, height: 1}}/>
+  )
+}
 
 class Example1 extends React.Component {
 
@@ -30,6 +35,36 @@ class Example1 extends React.Component {
             !leftActionActivated ?
               <Swipeable.Icon icon="icon-recyclebin" error onPress={action('clicked-button')} ></Swipeable.Icon> :
               <Swipeable.Icon icon="icon-success" success onPress={action('clicked-button')} ></Swipeable.Icon>
+          }
+        onRightActionActivate={() => this.setState({leftActionActivated: true})}
+        onRightActionDeactivate={() => this.setState({leftActionActivated: false})}
+        onRightActionComplete={() => this.setState({toggle: !toggle})}
+      >
+        <List.Item onPress={action('clicked-item')} icon="icon-persons">
+          <Text>With Swipe action</Text>
+        </List.Item>
+      </Swipeable>
+    );
+  }
+}
+
+class Example2 extends React.Component {
+
+  state = {
+    rightActionActivated: false,
+    toggle: false
+  };
+
+  render() {
+    const {leftActionActivated, toggle} = this.state;
+
+    return (
+      <Swipeable
+        rightActionActivationDistance={100}
+        rightContent={
+            !leftActionActivated ?
+              <Swipeable.Text error onPress={action('clicked-button')} >Delete</Swipeable.Text> :
+              <Swipeable.Text success onPress={action('clicked-button')} >Delete on Release</Swipeable.Text>
           }
         onRightActionActivate={() => this.setState({leftActionActivated: true})}
         onRightActionDeactivate={() => this.setState({leftActionActivated: false})}
@@ -66,6 +101,28 @@ storiesOf('Swipeable', module)
           <Text>With Buttons</Text>
         </List.Item>
       </Swipeable>
+      <Sep />
       <Example1 />
+      <Sep />
+      <Swipeable
+        leftButtonWidth={100}
+        rightButtonWidth={100}
+        rightButtons={[
+          <Swipeable.Text error onPress={action('clicked-button')} >Delete</Swipeable.Text>
+        ]}
+        leftButtons={[
+          <Swipeable.Text left info onPress={action('clicked-button')} >Permanently closed</Swipeable.Text>,
+          <Swipeable.Text left success onPress={action('clicked-button')} >Remote Open</Swipeable.Text>
+        ]}
+        // rightActionActivationDistance={140}
+        // onRightActionRelease={() => { action('release-right-item')() }}
+        >
+        <List.Item onPress={action('clicked-item')} icon="icon-persons">
+          <Text>With Buttons</Text>
+        </List.Item>
+      </Swipeable>
+      <Sep />
+      <Example2 />
+      <Sep />
     </View>
   ));
