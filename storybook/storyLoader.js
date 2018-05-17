5597deccca175f323aa8d2691fdb99034f413acc
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-05-17 14:42:56 +0200

    update deps

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,29 +5,52 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  
-  require('../js/atoms/Avatar.stories.js');
-  require('../js/atoms/Button.stories.js');
-  require('../js/atoms/Checkbox.stories.js');
-  require('../js/atoms/Icon.stories.js');
-  require('../js/atoms/IconButton.stories.js');
-  require('../js/atoms/ImageCropper.stories.js');
-  require('../js/atoms/Layout.stories.js');
-  require('../js/atoms/List.stories.js');
-  require('../js/atoms/Modal.stories.js');
-  require('../js/atoms/Notification.stories.js');
-  require('../js/atoms/QRCode.stories.js');
-  require('../js/atoms/Select.stories.js');
-  require('../js/atoms/Swipeable.stories.js');
-  require('../js/atoms/Textfield.stories.js');
-  require('../js/atoms/Typography.stories.js');
-  require('../js/molecules/Snack.stories.js');
-  require('../js/organisms/AnimatedAvatarView.stories.js');
-  require('../js/organisms/AnimatedLogin.stories.js');
-  require('../js/organisms/AnimatedPanelList.stories.js');
+  require('../js/atoms/Avatar.stories');
+  require('../js/atoms/Button.stories');
+  require('../js/atoms/Checkbox.stories');
+  require('../js/atoms/Icon.stories');
+  require('../js/atoms/IconButton.stories');
+  require('../js/atoms/ImageCropper.stories');
+  require('../js/atoms/Layout.stories');
+  require('../js/atoms/List.stories');
+  require('../js/atoms/Modal.stories');
+  require('../js/atoms/Notification.stories');
+  require('../js/atoms/QRCode.stories');
+  require('../js/atoms/Select.stories');
+  require('../js/atoms/Swipeable.stories');
+  require('../js/atoms/Textfield.stories');
+  require('../js/atoms/Typography.stories');
+  require('../js/molecules/Snack.stories');
+  require('../js/organisms/AnimatedAvatarView.stories');
+  require('../js/organisms/AnimatedLogin.stories');
+  require('../js/organisms/AnimatedPanelList.stories');
   
 }
 
+const stories = [
+  '../js/atoms/Avatar.stories',
+  '../js/atoms/Button.stories',
+  '../js/atoms/Checkbox.stories',
+  '../js/atoms/Icon.stories',
+  '../js/atoms/IconButton.stories',
+  '../js/atoms/ImageCropper.stories',
+  '../js/atoms/Layout.stories',
+  '../js/atoms/List.stories',
+  '../js/atoms/Modal.stories',
+  '../js/atoms/Notification.stories',
+  '../js/atoms/QRCode.stories',
+  '../js/atoms/Select.stories',
+  '../js/atoms/Swipeable.stories',
+  '../js/atoms/Textfield.stories',
+  '../js/atoms/Typography.stories',
+  '../js/molecules/Snack.stories',
+  '../js/organisms/AnimatedAvatarView.stories',
+  '../js/organisms/AnimatedLogin.stories',
+  '../js/organisms/AnimatedPanelList.stories',
+  
+];
+
 module.exports = {
   loadStories,
+  stories,
 };
