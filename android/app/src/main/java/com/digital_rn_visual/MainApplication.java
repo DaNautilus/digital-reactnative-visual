e28cfcfd2c8ae230e8318b000643575998628198
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-06-12 10:23:10 +0200

    update svg dep

--- android/app/src/main/java/com/digital_rn_visual/MainApplication.java
+++ android/app/src/main/java/com/digital_rn_visual/MainApplication.java
@@ -3,6 +3,7 @@ package com.digital_rn_visual;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SvgPackage(),
             new PickerPackage(),
             new RCTCameraPackage()
